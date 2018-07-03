b7e1ef35260e6f036e6dfa76b7aefdc0d6ecb7a2
AxiZ <AxiZ@WINDOWS-GQIIEKS>
2018-07-03 19:04:11 +0900

    Goods.java更新

--- Yamazon/src/main/java/yamazon/entity/Goods.java
+++ Yamazon/src/main/java/yamazon/entity/Goods.java
@@ -1,5 +1,78 @@
 package yamazon.entity;
 
 public class Goods {
+	private int goodsNumber;
+	private String goodsName;
+	private String goodsExplain;
+	private String goodsImage;
+	private int price;
+	private int taxPrice;
+	private String category;
+	private int stock;
+
+	public void setGoodsNumber(int goodsNumber) {
+		this.goodsNumber = goodsNumber;
+	}
+
+	public int getGoodsNumber() {
+		return this.goodsNumber;
+	}
+
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+
+	}
+
+	public String getGoodsName() {
+		return this.goodsName;
+	}
+
+	public void setGoodsExplain(String goodsExplain) {
+		this.goodsExplain = goodsExplain;
+	}
+
+	public String getGoodsExplain() {
+		return this.goodsExplain;
+	}
+
+	public void setGoodsImage(String goodsImage) {
+		this.goodsImage = goodsImage;
+	}
+
+	public String getGoodsImage() {
+		return this.goodsImage;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getPrice() {
+		return this.price;
+	}
+
+	public void setTaxPrice(int taxPrice) {
+		this.taxPrice = taxPrice;
+	}
+
+	public int getTaxPrice() {
+		return this.taxPrice;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getCategory() {
+		return this.category;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public int getStock() {
+		return this.stock;
+	}
 
 }
