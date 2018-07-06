371292df793c257b7d4f51b327c3df6058ce1bd3
AxiZ <AxiZ@WINDOWS-GQIIEKS>
2018-07-06 14:28:31 +0900

    menu遷移のメソッドにBindingResult追記

--- Yamazon/src/main/java/yamazon/controller/MenuController.java
+++ Yamazon/src/main/java/yamazon/controller/MenuController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,7 +27,7 @@ public class MenuController {
 	GoodsDao goodsDao;
 
 	@RequestMapping(value = { "/", "/menu" }, method = RequestMethod.GET)
-	public String menu(@ModelAttribute("yamazon")Search form,Model model) {
+	public String menu(@ModelAttribute("yamazon")Search form, BindingResult resurt, Model model) {
 		session.removeAttribute("manager");
 		List<Goods> goods = goodsDao.goodsMenu();
 		model.addAttribute("list", goods);
